b853861c3e685c309c005760797baad086cec5d4
shanmugarajsekar <shanmugaraj2003@gmail.com>
2024-03-01 16:23:41 +0530

    updated ui for blog

--- app/Components/cards/Blog.jsx
+++ app/Components/cards/Blog.jsx
@@ -2,56 +2,45 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Poppins } from "next/font/google";
 
 export default function Blog(props) {
   const DataSender = () => {
     ReceiveData(item);
   };
 
+
+
+
+  const blogDescription = props.des.length > 175 ? props.des.substr(0,175) + "..." : props.des;
   return (
-    // <div className="p-[10px] md:w-[32%] w-full  bg-[#212529] shadow-md hover:scale-[102.5%] duration-300">
-    //   <img
-    //     src={props.img}
-    //     className="object-cover w-full h-[200px]"
-    //     alt={props.title}
-    //   ></img>
-    //   <div className="mt-4 gap-2 flex flex-col text-white text-center p-2">
-    //     <p className="text-3xl font-Poppins font-semibold">{props.title}</p>
-    //     <p className=" font-Poppins font-light">{props.des}</p>
-    //     <p className=" font-Poppins font-light">{props.date}</p>
-    //     <Link href={props.to} className="text-white uppercase text-xs" onClick={()=>DataSender(item)}>
-    //       Read more
-    //     </Link>
-    //   </div>
-    // </div>
-    <div className="md:w-[30%] sm:w-full  bg-white p-3 rounded-lg hover:shadow-md">
+ 
+    <div className="md:w-[30%] sm:w-full h-[480px] bg-white p-3 rounded-lg shadow-md my-10 mx-2 ">
       <Link
         href={props.to}
         onClick={() => DataSender(item)}
-        className="text-black no-underline"
+        className="text-black no-underline flex flex-col justify-between h-[450px]"
       >
-        <img src={props.img} className=" rounded-lg"></img>
+        <img src={props.img} className=" rounded-lg h-[220px] w-full object-cover"></img>
         <div>
-          <p>{props.title}</p>
-          <p>{props.des}</p>
+          <p className="mb-0 text-2xl font-bold">{props.title}</p>
+          <p className="mb-0">{blogDescription}</p>
         </div>
         <div className="flex w-full">
           <div className="w-2/3 flex">
             <Image
-              src={
-                "https://img.freepik.com/free-vector/hand-drawn-collage-design_23-2149543516.jpg?w=1480&t=st=1704101631~exp=1704102231~hmac=c9e24da137942da4ff5a42a1aa083f817f348c78e318a081118b4365a48f45e6"
-              }
+              src= "/image.png"
               width={25}
               height={25}
               className="w-12 h-12 rounded-lg"
               alt="profile picture"
+              priority
             ></Image>
             <div>
-              <p>User Name</p>
+              <p className="mb-0">User Name</p>
+              <p className="mb-0">{props.date}</p>
             </div>
           </div>
-          <div className="w-1/3">{props.cat}</div>
+          <div className="w-1/3 flex justify-end">{props.cat}</div>
         </div>
       </Link>
     </div>
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,24 +1,25 @@
 "use client";
-import React, { useContext, useEffect, useState } from "react";
+import React, { Suspense, useContext, useEffect, useState } from "react";
 import RowCard from "../Components/cards/RowCard";
 import Heading from "../Components/mini-components/Heading";
 import rowData from "../data/blogContent.json";
 import Blog from "../Components/cards/Blog";
 import FilterButton from "../Components/mini-components/FilterButton";
 import { BlogContext } from "../context/BlogContext";
+import Loading from "./Loading";
 
 export default function Page() {
 
   const blog = useContext(BlogContext)
-
+ 
  
   return (
-    <>
+    <Suspense fallback={<Loading/>} >
       {blog && (
         <div className="w-full flex flex-col justify-center items-center md:p-10 p-5 flex-wrap bg-[#f0f3ff5f]">
           <Heading title={"Blog's"} />
 
-          <div className="flex flex-wrap justify-evenly w-full  items-center pt-3 gap-y-5">
+          <div className="flex flex-wrap justify-between md:w-[95%] w-full  items-center pt-3  pb-0">
             <FilterButton data={blog}  />
             {blog.length ? (
               blog.map((item) => (
@@ -41,6 +42,6 @@ export default function Page() {
           </div>
         </div>
       )}
-    </>
+    </Suspense>
   );
 }
