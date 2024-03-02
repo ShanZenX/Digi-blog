bf3c9e89cf4f6080dd5c7f2322348c4a662b3d7c
shanmugarajsekar <shanmugaraj2003@gmail.com>
2024-03-02 20:53:54 +0530

    added rte

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -8,21 +8,22 @@ import { Raleway } from "next/font/google";
 import { useState } from "react";
 import { db } from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
-import Link from "next/link";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 
 const fontRaleway = Raleway({
   weight: "600",
   style: "normal",
   subsets: ["latin"],
-});
+}); 
+
 
 export default function Page() {
-  useEffect(() => {
-    // const q =
-  });
+
+  const [value, setValue] = useState("");
+
+
 
   const [blogData, setBlogData] = useState({
     little_title: "",
@@ -58,17 +59,13 @@ export default function Page() {
   };
 
   const handelChange = (e) => {
-    const { name, value } = e.target;
-    setBlogData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+    
     console.log(e.target.value);
   };
 
   return (
     <div
-      className={`flex flex-col items-center justify-center  md:mx-40 md:my-24 bg-[#212529] text-white ${fontRaleway.className} `}
+      className={`flex flex-col items-center justify-center  md:mx-40 md:my-24 bg-white text-black ${fontRaleway.className} `}
     >
       <div className="pt-10 ">
         {" "}
@@ -76,145 +73,9 @@ export default function Page() {
       </div>
 
       <div className="w-full p-10 flex flex-col">
-        <Form className="flex flex-col" onSubmit={handelSubmit}>
-          <span className="flex md:flex-row   flex-col gap-7 mb-3">
-            <span className="md:w-1/2">
-              {" "}
-              <Form.Label to="Title">Title</Form.Label>
-              <Form.Control
-                type="text"
-                value={blogData.title}
-                name="title"
-                onChange={handelChange}
-                placeholder="A day in my life of office"
-                className="font-thin border-black focus:!shadow-lg"
-                required
-              />
-            </span>
-            <span className="md:w-1/2">
-              {" "}
-              <Form.Label to="SmallTitle">Small title</Form.Label>
-              <Form.Control
-                type="text"
-                value={blogData.little_title}
-                name="little_title"
-                onChange={handelChange}
-                placeholder="Daily blog"
-                className="font-thin border-black focus:!shadow-lg"
-                required
-              />
-            </span>
-          </span>
-          <span className="mb-3 flex flex-col">
-            <Form.Label to="description">Description</Form.Label>
-            <Form.Control
-              type="text"
-              rows={5}
-              value={blogData.description}
-              name="description"
-              onChange={handelChange}
-              placeholder="Description for your blog"
-              className="font-thin border-black focus:!shadow-lg "
-              required
-            />
-          </span>
-          <span className="mb-3 flex flex-col">
-            <Form.Label to="image">Image Url</Form.Label>
-            <Form.Control
-              type="text"
-              value={blogData.image_url}
-              name="image_url"
-              onChange={handelChange}
-              placeholder="Enter Url for image"
-              className="font-thin border-black focus:!shadow-lg "
-              required
-            />
-          </span>
-          <span className="md:flex gap-7 mb-3">
-            <span className="md:w-1/2 ">
-              <Form.Label to="SmallTitle">Select category</Form.Label>
-              <Form.Select
-                value={blogData.category}
-                name="category"
-                onChange={handelChange}
-                aria-label="Default select example"
-                className="font-thin border-black focus:!shadow-lg mb-3 md:mb-0"
-                required
-              >
-                <option>Select category</option>
-                <option value="Game">Game</option>
-                <option value="Tech">Tech</option>
-                <option value="Education">Education</option>
-                <option value="Food">Food</option>
-                <option value="Anime">Anime</option>
-                <option value="Travel">Travel</option>
-                <option value="Fitness">Fitness</option>
-                <option value="Finance">Fitness</option>
-                <option value="Health">Fitness</option>
-              </Form.Select>
-            </span>
-            <span className="md:w-1/2">
-              {" "}
-              <Form.Label to="SmallTitle">Select date</Form.Label>
-              <Form.Control
-                type="text"
-                value={blogData.date}
-                name="date"
-                onChange={handelChange}
-                placeholder="20,dec"
-                className="font-thin border-black focus:!shadow-lg"
-                required
-              />
-            </span>
-          </span>
-          <span className="mb-3 flex flex-col">
-            <Form.Label to="description-2">Description paragraph 1</Form.Label>
-            <Form.Control
-              type="text"
-              as="textarea"
-              rows={4}
-              value={blogData.additional_info}
-              name="additional_info"
-              onChange={handelChange}
-              placeholder="Description for your blog"
-              className="font-thin border-black focus:!shadow-lg "
-              required
-            />
-          </span>{" "}
-          <span className="mb-3 flex flex-col">
-            <Form.Label to="description-3">Description paragraph 2</Form.Label>
-            <Form.Control
-              type="text"
-              as="textarea"
-              rows={4}
-              value={blogData.additional_info_2}
-              name="additional_info_2"
-              onChange={handelChange}
-              placeholder="Description for your blog"
-              className="font-thin border-black focus:!shadow-lg "
-              required
-            />
-          </span>
-          <div className="flex w-full justify-between">
-            <Button variant="info" className="mr-4 mt-4 self-start">
-              <Link href={"/blog"} className=" text-white no-underline">
-                Go Back
-              </Link>
-            </Button>
-            <div className="justify-between">
-              <Button variant="danger" className="mr-4 mt-4">
-                <Link href={"/blog"} className=" text-white no-underline">
-                  Cancel
-                </Link>
-              </Button>
-              <Button variant="success" type="submit" className="mr-4 mt-4">
-                Publish
-              </Button>
-            </div>
-          </div>
-        </Form>
+      <ReactQuill theme="snow"  />
+
       </div>
-      <Editor></Editor>
     </div>
   );
 }
