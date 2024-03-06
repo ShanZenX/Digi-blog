c937c10265b430e69d71be7ace6dfecdf9ebcbcb
shanmugarajsekar <shanmugaraj2003@gmail.com>
2024-03-06 20:08:55 +0530

    .

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,18 +1,20 @@
-"use client"
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
-import { Raleway } from 'next/font/google';
-import { db } from '../firebase';
-import { collection, addDoc } from 'firebase/firestore';
+"use client";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import { Raleway } from "next/font/google";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
+import Heading from "../Components/mini-components/Heading";
+import { Form } from "react-bootstrap";
 
 const fontRaleway = Raleway({
-  weight: '600',
-  style: 'normal',
-  subsets: ['latin'],
+  weight: "600",
+  style: "normal",
+  subsets: ["latin"],
 });
 
 let DynamicJoditEditor;
-DynamicJoditEditor = dynamic(() => import('jodit-react'), { ssr: false });
+DynamicJoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
 // try {
 /// } catch (error) {
@@ -21,24 +23,22 @@ DynamicJoditEditor = dynamic(() => import('jodit-react'), { ssr: false });
 // }
 
 export default function Page() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   const [blogData, setBlogData] = useState({
-    little_title: '',
-    title: '',
-    description: '',
-    image_url: '',
-    category: '',
-    date: '',
-    additional_info: '',
-    additional_info_2: '',
+    little_title: "",
+    title: "",
+    description: "",
+    image_url: "",
+    category: "",
+    date: "",
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(blogData);
-    if (blogData.category !== '') {
-      await addDoc(collection(db, 'blogs'), {
+    if (blogData.category !== "") {
+      await addDoc(collection(db, "blogs"), {
         little_title: blogData.little_title,
         title: blogData.title,
         description: blogData.description,
@@ -48,9 +48,9 @@ export default function Page() {
         additional_info: blogData.additional_info,
         additional_info_2: blogData.additional_info_2,
       });
-      alert('Your blog created successfully');
+      alert("Your blog created successfully");
     } else {
-      alert('There is an issue in the website please try again');
+      alert("There is an issue in the website please try again");
     }
   };
 
@@ -60,7 +60,23 @@ export default function Page() {
   };
 
   return (
-    <div className={`flex flex-col items-center justify-center md:mx-40 md:my-24 bg-white text-black ${fontRaleway.className}`}>
+    <div
+      className={`flex flex-col  text-start justify-center md:mx-40 md:my-10 bg-white text-black ${fontRaleway.className}`}
+    >
+      <Heading title="Create post " />
+      <Form>
+        <span className="flex flex-wrap gap-2 justify-between">
+          <div className="w-[49%]">
+            <Form.Label className=" font-thin text-sm ">Title</Form.Label>
+
+            <Form.Control type="text" placeholder="Name your blog" />
+          </div>
+          <div className="w-[49%]">
+            <Form.Label>Little Title</Form.Label>
+            <Form.Control type="text" placeholder="Name your blog" />
+          </div>
+        </span>
+      </Form>
       <DynamicJoditEditor
         tabIndex={1}
         onBlur={(newContent) => setValue(newContent)}
